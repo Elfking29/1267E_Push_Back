f2e825629f77545d1f40a8f14c6b141d1a8a4d36
imaGeekyBoy <mpeirce09@dccs.org>
2025-11-07 16:47:13 -0500

    Make robot robot_config .h & .cpp

---
+++ include/robot_config.h
@@ -0,0 +1,3 @@
+using namespace pros;
+//Using extern in C++ isn't great practice, but it is easier and what VEXCode uses.
+extern pros::Controller Con1;
---
+++ src/robot_config.cpp
@@ -0,0 +1,5 @@
+#include "main.h"
+using namespace pros;
+
+// VEXCode Style Robot Configuration
+pros::Controller Con1(pros::E_CONTROLLER_MASTER);
